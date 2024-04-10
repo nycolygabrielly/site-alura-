344b8261f3c9863ed7fa435218755038a9b9a07a
nycoly gabrielly <144701230+nycolygabrielly@users.noreply.github.com>
2024-04-10 08:16:23 -0300

    Update style.cs

--- style.cs
+++ style.cs
@@ -1,2 +1,2 @@
-.principal{background-imagem:url("img/background.png");height.100vh,}
-.container_botão{bachgrounde-color:blue:color:whitedisplay:block}
+.principal{background-imagem:url("img/background.png");height.100vh}
+.container_botão{bachgrounde-color:blue; color:white; display:block}
