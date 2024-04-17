7aa585887ca85324185bc0abcff0e4a69dabe9eb
nycoly gabrielly <144701230+nycolygabrielly@users.noreply.github.com>
2024-04-17 11:12:16 -0300

    Update style.cs

--- style.cs
+++ style.cs
@@ -1,2 +1,40 @@
-.principal{background-imagem:url("img/background.png");height.100vh}
-.container_botão{bachgrounde-color:blue; color:white; display:block}
+:root{
+
+   --branco-principal:white;
+   --cinza-secondario: gray;
+   --botan-azul blue;
+--cord-fundo: black;
+}
+
+body{
+
+    background-color: black;
+    color: white;
+    font-size: 16px;
+    font-weight: 400;
+}
+
+.principal {
+    background-image: url("https://encrypted-tone.gstatic.com/images?q=tbn:ANd9GcQH0qnR9u1BLVFqgkizw@DohwJ7AjJN1PLcg4@RX4TXpHF_9FP3txIpk&s-18");
+
+    background-size: contain;
+    align-items: center;
+    text align: center;
+}
+
+
+.container botao {
+  background-color: var(botao-azul);
+  color:white;
+  border-radius: 5px;
+  display: block;
+  border: 2px solid var(-branco-principal);
+  text-decoration: none;
+margin-bottom: 1em;
+}
+
+
+.container, hight: 100wh;
+    display:grid;
+border-radius: 5px;
+
